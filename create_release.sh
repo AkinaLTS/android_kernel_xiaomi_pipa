8194c78f043d09d8c37ca86241c6f80449e3b92e
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-17 13:13:07 +0800

    fix typo

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-RELEASE_TIME=*$(date "+%Y-%m-%d_%H:%M:%S")""
+RELEASE_TIME=$(date "+%Y-%m-%d_%H:%M:%S")"
 mv Kernel_BloodReaper* BloodReaper-${RELEASE_TIME}.zip
 gh release create $(cat /proc/sys/kernel/random/uuid) --notes "Time: ${RELEASE_TIME}" --title ${RELEASE_TIME} BloodReaper-${RELEASE_TIME}.zip
