0c67a7d17faf78a937df03a537a53d81449e1d4e
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-17 10:29:37 +0800

    try to fix

--- buildm.sh
+++ buildm.sh
@@ -124,7 +124,7 @@ sed -i 's/\/\/39 01 00 00 11 00 03 51 03 FF/39 01 00 00 11 00 03 51 03 FF/g' ${d
     # ------------- Building for MIUI/HOS -------------
     echo "Clearing [out/] and building for MIUI/HOS....."
 
-    make $MAKE_ARGS pipa_Arkria_defconfig
+    make $MAKE_ARGS pipa_Arkria_defconfig lxc.config
 
 scripts/config --file out/.config \
     --set-str STATIC_USERMODEHELPER_PATH /system/bin/micd \
